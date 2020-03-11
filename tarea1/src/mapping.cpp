f24dcbea2d9b03e127b7b1277acbefebe73e9918
tpoggio <54154757+tpoggio@users.noreply.github.com>
2020-03-11 17:51:10 -0300

    mapping terminado

--- tarea1/src/mapping.cpp
+++ tarea1/src/mapping.cpp
@@ -13,18 +13,21 @@
 
 #include "utils.h"
 
-// Representación de 'TMapping'.
-// Se debe definir en mappingInfo.cpp.
-// struct repMap;
-// Declaración del tipo 'TMapping'.
-typedef struct repMap *TMapping;
+struct repMap {
+  nat asociaciones;
+  info_t pares[MAX + 1];
+};
 
 /*  Operaciones de TMapping */
 
 /*
   Devuelve un elemento de tipo 'TMapping' vacío (sin elementos).
  */
-TMapping crearMapping();
+TMapping crearMapping(){
+  TMapping map = new repMap;
+  (*map).asociaciones = 0;
+  return map;
+}
 
 /*
   Si en 'map' hay menos de MAX (definido en utils.h) asociaciones y 'clave'
@@ -32,24 +35,53 @@ TMapping crearMapping();
   (clave,valor)
   Devuelve 'map'.
  */
-TMapping asociar(nat clave, double valor, TMapping map);
+TMapping asociar(nat clave, double valor, TMapping map){
+  if( (*map).asociaciones < MAX && !esClave(clave, map)){
+    (*map).pares[asociaciones + 1].natural = clave;
+    (*map).pares[asociaciones + 1].real = valor;
+    (*map).asociaciones = (*map).asociaciones + 1;
+  };
+  return map;
+}
 
 /*
   Devuelve 'true' si y solo si 'clave' tiene un valor asociado en 'map'.
  */
-bool esClave(nat clave, TMapping map);
+bool esClave(nat clave, TMapping map){
+  while( nat i = 0 < (*map).asociaciones && (*map).pares[i].natural != clave){
+    i++;
+  };
+  return (*map).pares[i].natural == clave;
+}
 
 /*
   Devuelve el valor que en 'map' está asociado a 'clave'.
   Precondición: esClave(clave, map)
  */
-double valor(nat clave, TMapping map);
+double valor(nat clave, TMapping map){
+  while( nat i = 0 < (*map).asociaciones && (*map).pares[i].natural != clave){
+    i++;
+  };
+  return (*map).pares[i].natural;
+}
 
 /*
   Si 'clave' tiene un valor asociado en 'map' remueve de `map' la asociación de
   'clave'.
   Devuelve 'map'.
  */
-TMapping desasociar(nat clave, TMapping map);
+TMapping desasociar(nat clave, TMapping map){
+  if(esClave(clave)){
+    nat auxPos = 0;
+    while ( (*map).pares[auxPos].natural != clave ){
+      auxPos++;
+    }
+    for( nat i = auxPos; i < (*map).asociaciones; i++ ){
+      (*map).pares[i] = (*map).pares[i + 1] 
+    }
+    (*map).asociaciones --;
+  }
+  return map;
+}
 
 #endif
